7a9a91cd0da45dee2f83f77118903b4f81d2789e
laggingreflex <laggingreflex@gmail.com>
2017-08-20 10:15:47 +0530

    use fs-extra

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const os = require('os')
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
 const spawn = require('child_process').spawn
 
@@ -10,56 +10,63 @@ const cwd = p => path.join(process.cwd(), p || '.')
 const tmp = p => path.join(os.tmpdir(), 'npm-link-quick-' + (p || '') + '_' + Number(Date.now()))
 
 const pkg = require(cwd('package.json'))
-const orgPkg = fs.readFileSync(cwd('package.json'), 'utf8');
+const orgPkg = fs.readFileSync(cwd('package.json'), 'utf8')
 
 const keysToRemove = ['dependencies', 'devDependencies', 'optionalDependencies', 'peerDependencies', 'scripts']
 
-keysToRemove.forEach(k => modifyPkgJsonKey(pkg, k));
+keysToRemove.forEach(k => modifyPkgJsonKey(pkg, k))
 
 fs.writeFileSync(cwd('package.json'), JSON.stringify(pkg, null, 2))
 
+let renamedOldNodeModules
 try {
   fs.renameSync(cwd('node_modules'), cwd('node_modules-original'))
+  renamedOldNodeModules = true
 } catch (error) {}
 
 spawn(npm, ['link'], { stdio: 'inherit' }).on('close', () => {
   let err = null
 
-  try {
-    fs.unlinkSync(cwd('node_modules'))
-  } catch (error) {
-    fs.renameSync(cwd('node_modules'), cwd('node_modules-remove-me'))
-    const tmpdir = tmp(pkg.name)
+  if (renamedOldNodeModules) {
+    let removedTempNodeModules
     try {
-      // console.log(tmpdir);
-      fs.renameSync(cwd('node_modules-remove-me'), tmpdir)
-      try {
-        fs.unlinkSync(tmpdir)
-      } catch (error) {
-        err = true
-        console.warn(`Warning: Couldn't remove temporary node_modules dir:`, tmpdir, '\n', error.message, '\n Kindly remove it yourself.');
-      }
+      fs.removeSync(cwd('node_modules'))
+      removedTempNodeModules = true
     } catch (error) {
-      err = true
-      console.warn(`Warning: Couldn't remove temporary node_modules dir:`, cwd('node_modules-remove-me'), '\n', error.message, '\n Kindly remove it yourself.');
+      console.error(error)
+      console.warn(`Warning: Couldn't remove temporary node_modules dir`)
+      console.warn(`Please remove 'node_modules'`)
+      console.warn(`And rename 'node_modules-original' => 'node_modules'`)
     }
+
+    if (removedTempNodeModules) {
+      try {
+        fs.renameSync(cwd('node_modules-original'), cwd('node_modules'))
+      } catch (error) {
+        console.error(error)
+        console.warn(`Warning: Couldn't restore original node_modules dir`)
+        console.warn(`Please rename 'node_modules-original' => 'node_modules'`)
+      }
+    }
+
   }
 
   try {
-    fs.renameSync(cwd('node_modules-original'), cwd('node_modules'))
-  } catch (error) {}
+    fs.writeFileSync(cwd('package.json'), orgPkg)
+  } catch (error) {
+    console.error(error)
+    console.warn(`Warning: Couldn't restore original package.json`)
+    console.warn(`Please restore from git repo if possible`)
+  }
 
-  fs.writeFileSync(cwd('package.json'), orgPkg)
-
-  console.log('Done!');
-
+  console.log('Done!')
 })
 
 
 function modifyPkgJsonKey(pkg, key) {
-  let tmp = key + '_npm_quick_link_tmp';
+  let tmp = key + '_npm_quick_link_tmp'
   if (pkg[key]) {
-    pkg[tmp] = pkg[key];
-    delete pkg[key];
+    pkg[tmp] = pkg[key]
+    delete pkg[key]
   }
 }
